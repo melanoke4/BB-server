f3a0e7ad90aa21c2ab641aa5cb2763724e902cc6
Melanie Warwick <princessmelanoke@gmail.com>
2024-09-13 12:07:28 -0500

    updated models, fixed itemSerializer

--- bottomlessboxapi/models/lore.py
+++ bottomlessboxapi/models/lore.py
@@ -2,6 +2,6 @@ from django.db import models
 from bottomlessboxapi.models.item import Item
 
 class Lore(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='lores')
+    item = models.OneToOneField(Item, on_delete=models.CASCADE, related_name='lore')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- bottomlessboxapi/views/item.py
+++ bottomlessboxapi/views/item.py
@@ -7,7 +7,11 @@ from bottomlessboxapi.models.category import Category
 from bottomlessboxapi.models.item import Item
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
+from bottomlessboxapi.models.lore import Lore
+from bottomlessboxapi.models.review import Review
 from bottomlessboxapi.models.user import User
+from bottomlessboxapi.views.lore import LoreSerializer
+from bottomlessboxapi.views.review import ReviewSerializer
 
 
 
@@ -37,7 +41,7 @@ class ItemViewSet(viewsets.ViewSet):
 
     def retrieve(self, request, pk=None):
         try:
-            item = get_object_or_404(Item, pk=pk, user=request.user)
+            item = get_object_or_404(Item, pk=pk, user=request.query_params.get('user_id'))
             serializer = ItemSerializer(item)
             return Response(serializer.data)
         except ObjectDoesNotExist:
@@ -66,11 +70,13 @@ class ItemSerializer(serializers.ModelSerializer):
     category_names = serializers.SerializerMethodField()
     location_name = serializers.ReadOnlyField(source='location.name')
     status_name = serializers.ReadOnlyField(source='status.name')
+    lore = LoreSerializer(required=False, allow_null=True)
+    review = ReviewSerializer(required=False, allow_null=True)
 
     class Meta:
         model = Item
         fields = ['id', 'user', 'name', 'cost', 'purchase_date', 'categories', 'category_names',
-                  'location', 'location_name', 'status', 'status_name', 'image_url']
+                  'location', 'location_name', 'status', 'status_name', 'image_url', 'lore', 'review']
         read_only_fields = ['id', 'user']
 
     def get_category_names(self, obj):
@@ -78,13 +84,57 @@ class ItemSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         categories = validated_data.pop('categories', [])
+        lore_data = validated_data.pop('lore', None)
+        review_data = validated_data.pop('review', None)
+        
         item = Item.objects.create(**validated_data)
         item.categories.set(categories)
+        
+        if lore_data:
+            Lore.objects.create(item=item, **lore_data)
+        
+        if review_data:
+            Review.objects.create(item=item, **review_data)
+        
+        # Refresh the item instance to include the newly created lore and review
+        item.refresh_from_db()
         return item
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if hasattr(instance, 'lore') and instance.lore:
+            representation['lore'] = LoreSerializer(instance.lore).data
+        if hasattr(instance, 'review') and instance.review:
+            representation['review'] = ReviewSerializer(instance.review).data
+        return representation
 
     def update(self, instance, validated_data):
         categories = validated_data.pop('categories', None)
+        lore_data = validated_data.pop('lore', None)
+        review_data = validated_data.pop('review', None)
+        
         instance = super().update(instance, validated_data)
+        
         if categories is not None:
             instance.categories.set(categories)
+        
+        if lore_data:
+            if instance.lore:
+                for attr, value in lore_data.items():
+                    setattr(instance.lore, attr, value)
+                instance.lore.save()
+            else:
+                lore = Lore.objects.create(**lore_data)
+                instance.lore = lore
+        
+        if review_data:
+            if instance.review:
+                for attr, value in review_data.items():
+                    setattr(instance.review, attr, value)
+                instance.review.save()
+            else:
+                review = Review.objects.create(**review_data)
+                instance.review = review
+        
+        instance.save()
         return instance
